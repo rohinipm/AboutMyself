9c76dc23d612f3723c497373e03835e5f3a0f208
Rohiin <mrohini216@gmail.com>
2021-08-26 22:54:38 +0530

    docs: initial code

---
+++ aboutMyself.js
@@ -0,0 +1,7 @@
+var readlineSync=require('readline-Sync');
+
+console.log("\t\t Do you know about me (Rohini)");
+console.log("\t\t-------------------------------");
+
+var uname=readlineSync.question("May I know your name:");
+console.log("Welcome "+uname);
